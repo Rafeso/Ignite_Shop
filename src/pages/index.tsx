5ae2e6662c4072aadee3706872e73e65cc7bc891
Rafael Feitosa <rafaefeitosa@gmail.com>
2023-03-11 13:01:09 -0300

    feat(ssg): :sparkles: stripe integration - using ssg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,11 @@ import { HomeContainer, Product } from "@styles/home";
 import Image from "next/image";
 import { useKeenSlider } from "keen-slider/react";
 
-import camiseta1 from "../assets/1.png";
-import camiseta2 from "../assets/2.png";
-import camiseta3 from "../assets/3.png";
-
 import "keen-slider/keen-slider.min.css";
 
 import Stripe from "stripe";
 import { stripe } from "@/lib/stripe";
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 
 interface HomeProps {
   products: {
@@ -46,7 +42,7 @@ export default function Home({ products }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ["data.default_price"],
   });
@@ -66,5 +62,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       products,
     },
+    revalidate: 60 * 60 * 2, // 2 hours
   };
 };
