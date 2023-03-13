f3535950d58cc175e4db4963eafc49bc834f04b6
Rafael Feitosa <rafaefeitosa@gmail.com>
2023-03-13 17:50:14 -0300

    feat(header): :sparkles: added link to home in logo image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { globalStyles } from "@/styles/global";
 import { Container, Header } from "@styles/app";
 import type { AppProps } from "next/app";
 import Image from "next/image";
+import Link from "next/link";
 
 import logoImg from "../assets/logo.svg";
 
@@ -11,7 +12,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <Image src={logoImg} alt="" width={130} height={52} />
+        <Link href="/">
+          <Image src={logoImg} alt="" width={130} height={52} />
+        </Link>
       </Header>
       <Component {...pageProps} />
     </Container>
